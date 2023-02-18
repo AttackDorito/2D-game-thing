7161f33f3f383503c016ce77348787be2a511e49
AttackDorito <mwk.orourke@hotmail.com>
2023-02-18 21:05:12 +0000

    new things

--- sketch.js
+++ sketch.js
@@ -1,11 +1,17 @@
 let screen_width = window.innerWidth;
 let screen_height = window.innerHeight;
+var gameObjects = []
 
 function setup() {
   createCanvas(screen_width, screen_height);
+  gameObjects = [new objQuad([0,0],[100,0],[100,100],[0,100])];
+  console.log(gameObjects[0])
 }
 
 function draw() {
   background(220);
   ellipse(screen_width/2,screen_height/2,80,80);
+  gameObjects.forEach(function(gameObject) {
+    gameObject.drawObj();
+  })
 }
--- the_game.js
+++ the_game.js
@@ -1,26 +1,49 @@
 
 const playfieldDimensions = 2048
 
+const scaleFactor = 1
+
+const viewObj = {
+    posX        : 200,
+    posY        : 200,
+    velocityVect: [0,0], //velocity vector
+    direction   : 0,         //facing direction (radians)
+    thrust      : function thrust(){
+        this.velocityVect[0] += Math.sin(this.direction);
+        this.velocityVect[1] += Math.cos(this.direction);
+    }
+}
+
+function callDraw(objectToDraw){
+    objectToDraw.drawObj();
+}
+
 class objQuad {
     constructor(v1,v2,v3,v4) {
         this.v1 = v1;
         this.v2 = v2;
         this.v3 = v3;
         this.v4 = v4;
+
+        this.screen1 = convertPoint(this.v1);
+        this.screen2 = convertPoint(this.v2);
+        this.screen3 = convertPoint(this.v3);
+        this.screen4 = convertPoint(this.v4);
     }
 
+    drawObj(){
+        this.screen1 = convertPoint(this.v1);
+        this.screen2 = convertPoint(this.v2);
+        this.screen3 = convertPoint(this.v3);
+        this.screen4 = convertPoint(this.v4);
+        fill(51);
+        quad(this.screen1[0],this.screen1[1],this.screen2[0],this.screen2[1],this.screen3[0],this.screen3[1],this.screen4[0],this.screen4[1])
+    }
 }
 
-var gameObjects = []
-
-const scaleFactor = 50
-
-const camera = {
-    posX: 0,
-    posY: 0
-}
-
-function convertPoint(x,y){
-    x = Math.floor(x - camera.posX/scaleFactor) + Math.floor(screen_width / 2)
-    y = Math.floor(y- camera.posY/scaleFactor) + Math.floor(screen_height / 2)
+function convertPoint(vert){
+    var vertOut = []
+    vertOut[0] = Math.floor((vert[0] - viewObj.posX) * scaleFactor) + Math.floor(screen_width / 2)
+    vertOut[1] = Math.floor((vert[1] - viewObj.posY) * scaleFactor) + Math.floor(screen_height / 2)
+    return vertOut;
 }
