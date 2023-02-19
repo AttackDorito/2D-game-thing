e46f428d91edfc175fc5cf6e57fe96e415cac60a
Shadow Kestrel <106026982+shadow-kestrel@users.noreply.github.com>
2023-02-19 05:57:20 +0000

    untested again. test me uwu

--- collide.js
+++ collide.js
@@ -1,3 +1,18 @@
-function collide_line () {
-    // TODO
+function check_collide_line (line_to_check) {
+    let cum = 0;
+    const normal = get_normal_vector(line_to_check);
+    const offset = normal[0] * line_to_check[0] + normal[1] * line_to_check[1];
+    for (let v of playerObj.vertexPoints) {
+        cum += Math.sign(normal[0] * v[0] + normal[1] * v[1] - offset)
+    }
+    return Math.abs(cum) != 3;
+}
+
+function get_normal_vector (a_line) {
+    const scale_vector = [
+        a_line.v1[1] - a_line.v1[1],
+        a_line.v2[0] - a_line.v2[0]
+    ];
+    const mag = sqrt(a_line[1]**2 + a_line[2]);
+    return scale_vector.map(x => x/mag);
 }
